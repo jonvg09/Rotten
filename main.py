684fded9810747f7457d76cf62edb3d653da2951
Jón <jonvg09@ru.is>
2014-10-28 12:58:07 +0000

    comment og aðeins meira.

--- main.py
+++ main.py
@@ -3,41 +3,37 @@ from pprint import pprint
 
 def main():
 	with open('rotten_test.csv', 'rb') as csvfile:
-		freq_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+		training_set_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
 		review_list = []
 		review = ()
-		for row in freq_reader:
-			review = row[0], row[1]
+		for row in training_set_reader:
+			review = row[0], row[1]					#0 row[0]: reviews #row[0]: rate
 			review_list.append(review)
 
 		list_of_words = []
 		unique_dict = {}
 		for row in review_list:
-			temp_list_of_words = row[0].split()
+			temp_list_of_words = row[0].split()		#splits up the sentences into list of words
 			for temp_word in temp_list_of_words:
 				list_of_words.append(temp_word)
 			
 		for word in list_of_words:
-			unique_dict[word] = 0.0
+			unique_dict[word] = 0.0					#no duplicates in dict
 
 		for k, v in unique_dict.iteritems():
 			word_count = 0
 			sum_rate = 0
-			for sentence in review_list:
+			for sentence in review_list:			#counts the word in training set and finds out the cumulative rate for all words
 				temp_list_of_words = sentence[0].split()
 				for word in temp_list_of_words:
 					if k == word:
 						word_count = word_count + 1
-						#print sentence[1]
 						sum_rate = sum_rate + float(sentence[1]) 
 			
-			unique_dict[k]= sum_rate / word_count
+			unique_dict[k]= sum_rate / word_count	#average rate
 
 		pprint(unique_dict)
 
-#		fyrsta_lina = review_list[0]
-#		print fyrsta_lina[0].split()
-#		#print fyrsta_lina[1]
 
 if __name__ == "__main__":
 	main()
